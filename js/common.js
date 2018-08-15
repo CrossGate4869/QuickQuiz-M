c89c6eb23c160fceb31ed0656666a10c751bf1b2
南郊居士 <njjs1991@163.com>
2018-08-15 09:30:15 +0800

    首次提交

---
+++ js/common.js
@@ -0,0 +1,216 @@
+﻿var xmlMain;
+
+function loadXML(strURL)
+{
+	if (!strURL)
+	{
+		return null;
+	}
+	
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  	xmlhttp = new XMLHttpRequest();
+  }
+	else
+  {// code for IE6, IE5
+  	xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  
+  if (!xmlhttp)
+  {
+  	alert("解析模块不可使用!")
+  	return null;
+  }
+  
+	xmlhttp.open("GET", strURL, false);
+	xmlhttp.send();
+	
+	return xmlhttp.responseXML;
+}
+
+function getURLPara(url, paraStr)
+{
+	if (!url || !paraStr)
+	{
+		return null;
+	}
+	
+	var idx = url.indexOf("?");
+	if (idx <= 0)
+	{
+		return null;
+	}
+	
+	var strAll = url.substr(idx + 1);
+	var strList = strAll.split("&");
+	
+	for (var i = 0; i < strList.length; i++)
+	{
+		if (strList[i].indexOf(paraStr) >= 0)
+		{
+			return strList[i].split("=")[1];
+		}
+	}
+	
+	return null;
+}
+
+function matchQstn(qstn, txt)
+{
+	if (qstn.getElementsByTagName("desc")[0].childNodes[0].nodeValue.indexOf(txt) >= 0)
+	{
+		return true;
+	}
+	
+	switch(qstn.getAttribute("type"))
+	{
+	case "s-choise":
+	case "m-choise":
+		if (matchChildValue(qstn, "opt", txt))
+		{
+			return true;
+		}
+		break;
+		
+	case "blank":
+		if (matchChildValue(qstn, "blk", txt))
+		{
+			return true;
+		}
+		break;
+		
+	case "text":
+		if (qstn.getElementsByTagName("ans")[0].childNodes[0].nodeValue.indexOf(txt) >= 0)
+		{
+			return true;
+		}
+		break;
+	}
+	
+	return false;
+}
+
+function matchChildValue(qstn, tag, txt)
+{
+	var optList = qstn.getElementsByTagName(tag);
+	for (var i = 0; i < optList.length; i++)
+	{
+		if (optList[i].childNodes[0].nodeValue.indexOf(txt) >= 0)
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+function showHideShowFunc(elementId, textId, textTag)
+{
+	var qstnSet = document.getElementById(elementId);
+	if (!qstnSet.style.display)
+	{
+		qstnSet.style.display = "none";
+		document.getElementById(textId).innerHTML = "&#9658" + textTag;
+	}
+	else
+	{
+		qstnSet.style.display = "";
+		document.getElementById(textId).innerHTML = "&#9660" + textTag;
+	}
+}
+
+function localStorageEnable()
+{
+	if(window.localStorage)
+	{
+		try
+		{
+			localStorage.setItem("key", "value");
+			localStorage.removeItem("key");
+			return true;
+		}
+		catch(e)
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
+}
+
+Math.seededRandom = function(max, min)
+{
+	max = max || 1;
+	min = min || 0;
+
+	Math.seed = (Math.seed * 9301 + 49297) % 233280;
+	var rnd = Math.seed / 233280.0;
+
+	return Math.ceil( min + rnd * (max - min) );
+}
+
+function bkdrHash(string)
+{
+    var seed = 131; // 31 131 1313 13131 131313 etc..
+    var hash = 0, i = 0;
+	
+    while(i < string.length)
+    {
+        hash = hash * seed + string.charCodeAt(i);
+        i++;
+    }
+	
+    return (hash % 0x7FFFFFFF);
+}
+
+function setCookie(c_name, value, expirehours)
+{
+	if (!navigator.cookieEnabled)
+	{
+		return;
+	}
+	
+	var exdate = new Date();
+	exdate.setTime(exdate.getTime() + expirehours * 60 * 60 * 1000);
+	document.cookie = c_name + "=" + escape(value) + ((!expirehours) ? "" : (";expires=" + exdate.toGMTString())) + ";path=/";
+}
+
+function getCookie(name)
+{
+	var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+	if(arr = document.cookie.match(reg))
+	{
+		return unescape(arr[2]);
+	}
+	else
+	{
+		return null;
+	}
+}
+
+function delCookie(name)
+{
+	var exp = new Date();
+	exp.setTime(exp.getTime() - 1);
+	var cval = getCookie(name);
+	if(cval)
+	{
+		document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString() + ";path=/";
+	}
+}
+
+function getIndex(list, item)
+{
+	for (var i = 0; i < list.length; i++)
+	{
+		if (item == list[i])
+		{
+			return i;
+		}
+	}
+	
+	return 0;
+}
---
+++ js/index.js
@@ -0,0 +1,228 @@
+﻿
+var xmlList;
+
+function indexNightMode(tip)
+{
+	nightMode(tip);
+	
+	if (night)
+	{
+		document.getElementById("titleicon").src = "img/iconnight.png";
+	}
+	else
+	{
+		document.getElementById("titleicon").src = "img/icon.png";
+	}
+}
+
+function initIndex()
+{
+	indexNightMode(false);
+	
+	// 拉取题目列表
+	xmlList = loadXML("http://GXSoftware.coding.me/QuickQuiz-M/xml/list.xml");
+	if (!xmlList)
+	{
+		document.write("<p>题目列表链接可能已经失效，请等待修复或反馈...</p><p>如需反馈，请关注微信公众号“热控团青在线”后留言</p>");
+		return;
+	}
+	
+	var key = xmlList.getElementsByTagName("key");
+	if (!key || key[0].childNodes[0].nodeValue != "QuickQuiz-List")
+	{
+		document.write("<p>题目列表链接可能已经被破坏，请等待修复或反馈...</p><p>如需反馈，请关注微信公众号“热控团青在线”后留言</p>");
+		return;
+	}
+
+	// 重建题目组列表
+	var list = xmlList.getElementsByTagName("tag");
+	var groupList = document.getElementById("selectGrp");
+	
+	var i;
+	for (i = groupList.length - 1; i >= 0; i--)
+	{
+		groupList.remove(i);
+	}
+	
+	var y;
+	for (var i = 0; i < list.length; i++)
+	{
+		y = document.createElement('option');
+		y.text = list[i].childNodes[0].nodeValue;
+		groupList.add(y);
+	}
+	
+	// 建立题目列表
+	changeGrp();
+	
+	initSel();
+	changeSel();
+	
+	// 尝试解决Safari回滚后不能回到顶端的问题（不一定能解决）
+	window.scrollTo(0, 0);
+}
+
+function getSelLib()
+{
+	var selectIdx = document.getElementById("selectGrp").selectedIndex;
+	var selectGrp = xmlList.getElementsByTagName("group")[selectIdx];
+	
+	selectIdx = document.getElementById("selectLib").selectedIndex;
+	return selectGrp.getElementsByTagName("item")[selectIdx];
+}
+
+function setBtnHref(btn)
+{
+	if (!checkPassword())
+	{
+		return;
+	}
+	
+	var url = btn.value;
+	if (!url)
+	{
+		return;
+	}
+	
+	var selLib = getSelLib();
+	var hrefStr = selLib.getElementsByTagName("href")[0];
+	if (!hrefStr)
+	{
+		alert("题目列表链接可能已经被破坏，请等待修复或反馈...");
+		return;
+	}
+	
+	setCookie("lib", hrefStr.childNodes[0].nodeValue, 720);
+	url = url + "?" + "href=" + hrefStr.childNodes[0].nodeValue;
+	
+	// 如果需要输入密码，则在url中也加入密码
+	if(document.getElementById("passdiv").style.display != "none")
+	{
+		url = url + "&" + "pass=" + document.getElementById("password").value;
+		setCookie("pass", document.getElementById("password").value, 168);
+	}
+	else
+	{
+		delCookie("pass");
+	}
+	
+	location.href = url;
+}
+
+function initSel()
+{
+	var href = getCookie("lib");
+	if (!href)
+	{
+		return;
+	}
+	
+	var hrefList = xmlList.getElementsByTagName("href");
+	for (var i = 0; i < hrefList.length; i++)
+	{
+		// 找到了对应的题库
+		if (href == hrefList[i].childNodes[0].nodeValue)
+		{
+			var item = hrefList[i].parentNode;
+			var group = item.parentNode;
+			
+			var j = getIndex(xmlList.getElementsByTagName("group"), group);
+			if (j < document.getElementById("selectGrp").length)
+			{
+				document.getElementById("selectGrp").selectedIndex = j;
+			}
+			
+			changeGrp();
+			break;
+		}
+	}
+}
+
+function changeSel()
+{
+	var selectLib = getSelLib();
+	document.getElementById("passdiv").style.display =
+		(selectLib.getElementsByTagName("password").length) ? "" : "none";
+		
+	if (selectLib.getElementsByTagName("password").length)
+	{
+		var passSave = getCookie("pass");
+		if (passSave)
+		{
+			document.getElementById("password").value = passSave;
+		}
+	}
+}
+
+function checkPassword()
+{
+	var selectLib = getSelLib();
+	var passwordNode = selectLib.getElementsByTagName("password");
+	if (passwordNode.length)
+	{
+		var userStr = makeMD5(document.getElementById("password").value);
+		if (userStr != passwordNode[0].childNodes[0].nodeValue)
+		{
+			alert("密码错误！");
+			document.getElementById("password").value = "";
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	else
+	{
+		return true;
+	}
+}
+
+function changeGrp()
+{
+	var selectIdx = document.getElementById("selectGrp").selectedIndex;
+	var selectGrp = xmlList.getElementsByTagName("group")[selectIdx];
+	var selectList = document.getElementById("selectLib");
+	var list = selectGrp.getElementsByTagName("desc");
+	
+	var i;
+	for (i = selectList.length - 1; i >= 0; i--)
+	{
+		selectList.remove(i);
+	}
+	
+	var y;
+	for (var i = 0; i < list.length; i++)
+	{
+		y = document.createElement('option');
+		y.text = list[i].childNodes[0].nodeValue;
+		selectList.add(y);
+	}
+	
+	// 如果曾经保存了浏览过的题库，则在选中对应题目组后自动选中
+	var href = getCookie("lib");
+	if (href)
+	{
+		var hrefList = selectGrp.getElementsByTagName("href");
+		for (var i = 0; i < hrefList.length; i++)
+		{
+			if (href == hrefList[i].childNodes[0].nodeValue)
+			{
+				document.getElementById("selectLib").selectedIndex = i;
+				break;
+			}
+		}
+	}
+	
+	changeSel();
+}
+
+window.onpageshow = function(event)
+{
+	if (event.persisted)
+	{
+		window.location.reload();
+		// 尝试解决Safari回滚后不能回到顶端的问题（不一定能解决）
+		window.scrollTo(0, 0);
+	}
+}
